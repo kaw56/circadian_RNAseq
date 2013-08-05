5b8a159b8d26816bfc37a767c4f976e5a656834d
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-08-05 17:37:21 +0100

    EdgeR pairwise comparisons
    This may prove to be total nonsense but I don't want
    to run bowtie again to try cuffdiff right now

--- 2_clean.R
+++ 2_clean.R
@@ -8,3 +8,15 @@ library(reshape2)
 names(counts) <- c("cluster", "CT0","CT3","CT6","CT9","CT12","CT15","CT18","CT21")
 
 counts_long <- melt(counts, id.vars="cluster", variable.name="time",value.name="readcount")
+
+# rejig for playing with edgeR
+gene_list <- counts[1]
+
+# pairwise comparisons to CT0
+CT3  <- counts[,2:3]
+CT6  <- counts[,c(2,4)]
+CT9  <- counts[,c(2,5)]
+CT12 <- counts[,c(2,6)]
+CT15 <- counts[,c(2,7)]
+CT18 <- counts[,c(2,8)]
+CT21 <- counts[,c(2,9)]
--- 4_do.R
+++ 4_do.R
@@ -1,10 +1,20 @@
 # perform analysis here generate graphs and tables and so on
 library(ggplot2)
+library(edgeR)
 
-# ridiculous raw plotting time (looking at general trend)
+# ridiculous raw plotting time (looking at general trend not normalised or anything)
 ridiculous <- ggplot(counts_long, aes(x=time, 
                   y=readcount, 
                   group=cluster))  + 
     geom_line()
 
 ridiculous
+
+# putting into edgeR-type format
+diff_expr_CT3 <- DGEList(counts=CT3)
+diff_expr_CT6 <- DGEList(counts=CT6)
+diff_expr_CT9 <- DGEList(counts=CT9)
+diff_expr_CT12 <- DGEList(counts=CT12)
+diff_expr_CT15 <- DGEList(counts=CT15)
+diff_expr_CT18 <- DGEList(counts=CT18)
+diff_expr_CT21 <- DGEList(counts=CT21)
